2ad2a7ffbc59a5d6910fe1b03a58a3efcd71b2a5
Tejal Dadaji pagar <tejalpagar368@gmail.com>
2026-08-10 01:47:35 +0530

    Refactor day01.py with consistent formatting

--- day01.py
+++ day01.py
@@ -16,3 +16,20 @@ print(type(str1))
 
 #List  
 lst=[1,'abc','sabnam',123]
+
+# Conditional Statements
+#if else block
+
+Students =["Ahan","Riya","Aryan","Khushi","Mahima","sunil","Neeta"]
+Attendance =["Ahan","Aryan","Khushi","mahima","Neeta"]
+
+stud = input ("Enter Student Name : ")
+
+if stud in Students:
+    if stud not in Attendance:
+        print(stud,"is Not present")
+    else:
+        print(stud,"is Present")
+
+else:
+    print("This Student is not from class")
