dfa5780c93f5dd55d5dfebf7a48061d0a2c9ac39
Tejal Dadaji pagar <tejalpagar368@gmail.com>
2026-08-09 23:57:55 +0530

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,18 @@
+# Variable  an Data types
+x = 10 #int
+y = 20 #int
+print(x+y)
+
+#typecasting
+
+x="123"
+x=int(x)
+print(type(x)) #converting string to int
+num =123
+str1 =str(num)
+print(str1)
+print(type(str1))
+
+
+#List  
+lst=[1,'abc','sabnam',123]
