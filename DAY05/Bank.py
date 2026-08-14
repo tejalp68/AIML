bec6acedf1e3561f35fdf29bec72d40ec11e7361
tejalp68 <tejalpagar368@gmail.com>
2026-08-14 14:05:08 +0530

    created new file of bank.py

---
+++ DAY05/Bank.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self,owner,balance):
+        self.owner =owner
+        self.balance =balance
+    def deposit(self,amount):
+        self.balance += amount
+    def withdraw(self,amount):
+        if amount <= self.balance :
+            self.balance -= amount
+        else:
+            print("Insufficient balance")
+    def show_balance(self):
+        print(f"Balance is :{self.balance}" )
+account =BankAccount('Tejal',5000)
+account.deposit(1500)
+account.withdraw(1000)
+account.show_balance()
